bacc2e1540f0754039c46fe28bf426df2e6f78fe
Matheus Gabriel Polim da Silva <matheusgpolim@gmail.com>
2024-08-19 20:11:58 -0300

    Utilizando nodemailer para recuperacao de senha na aplicacao e resolvendo erro de chamada

--- src/shared/providers/MailProvider/implementations/EtherealMailProvider.ts
+++ src/shared/providers/MailProvider/implementations/EtherealMailProvider.ts
@@ -1,7 +1,33 @@
 import IMailProvider from "../models/IMailProvider";
+import nodemailer, { Transporter } from 'nodemailer';
 
 class EtherealMailProvider implements IMailProvider {
+  private client: Transporter
+
+  constructor() {
+    nodemailer.createTestAccount().then(account => {
+      this.client = nodemailer.createTransport({
+        host: account.smtp.host,
+        port: account.smtp.port,
+        secure: account.smtp.secure,
+        auth: {
+          user: account.user,
+          pass: account.pass
+        }
+      });
+    });
+  }
+
   public async sendMail(to: string, body: string): Promise<void> {
+    const message = await this.client.sendMail({
+      from: 'equipe GoBarber <equipe@gobarber.com.br>',
+      to,
+      subject: 'Recuperacao de senha',
+      text: body,
+    });
+
+    console.log('Message sent: %s', message.messageId);
+    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(message));
   };
 };
 
